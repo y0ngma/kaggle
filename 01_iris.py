7e8feee6b83f7fba64d4f6af5397a2c3925e3568
y0ngma <takeit2sy@pusan.ac.kr>
2022-03-28 19:06:28 +0900

    iris 데이터 다운로드

--- 01_iris.py
+++ 01_iris.py
@@ -3,4 +3,17 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 from subprocess import check_output
-# print(check_output(["ls", "../input"]).decode("utf8"))
+import os
+
+PROJECT_DIR = os.path.abspath(__file__)
+data_folder = f"{os.path.dirname(PROJECT_DIR)}/data"
+data_path = f"{data_folder}/Iris.csv"
+
+df = pd.read_csv(data_path)
+# print(df.head())
+
+print(data_folder)
+# print(os.listdir(data_folder))
+
+# print(check_output(["ls", data_folder]).decode("utf8"))
+print(check_output(["ls", "c:/home/jason/kaggle/data"]).decode("utf8"))
